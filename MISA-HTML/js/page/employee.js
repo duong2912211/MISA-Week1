1c486b7fe169e415cff6d37d1bd3c9acbe53617c
duong2912211 <73048362+duong2912211@users.noreply.github.com>
2020-12-26 08:25:07 +0700

    first commit

---
+++ MISA-HTML/js/page/employee.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    loadData();
+})
+
+function loadData() {
+    //Lay du lieu ve 
+    $.ajax({
+        url: "http://api.manhnv.net/api/employees",
+        method: "GET",
+    }).done(function (res) {
+        var data = res;
+        $.each(data, function (index, item) {
+            var tr = $(`<tr>
+                            <td><div><span>`+ item.EmployeeCode +`</span></div></td>
+                            <td><div><span>`+ item['FullName'] +`</span></div></td>
+                            <td><div><span>`+ item['GenderName'] +`</span></div></td>
+                            <td><div><span>`+ item['DateOfBirth'] +`</span></div></td>
+                            <td><div><span>`+ item['PhoneNumber'] +`</span></div></td>
+                            <td><div><span>`+ item['Email'] +`</span></div></td>
+                            <td><div><span>`+ item['PositionName'] +`</span></div></td>
+                            <td><div><span>`+ item['DepartmentName'] +`</span></div></td>
+                            <td><div><span>`+ item['Salary'] +`</span></div></td>
+                            <td><div><span>`+ item['Address'] +`</span></div></td>
+                            <td><div><span>`+ item['WorkStatusName'] +`</span></div></td>       
+                        </tr>`);
+            $('table tbody').append(tr);
+        })
+
+    }).fail(function (res) {
+
+    })
+    //binding du lieu len table
+}
